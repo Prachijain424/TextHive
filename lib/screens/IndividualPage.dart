662eca028832a44ef139cd39fe9b9c6087aba9f6
iamlalit <alalitkumar02@gmail.com>
2023-01-24 14:44:49 +0530

    Added all UI of Individual ChatPage

--- lib/screens/IndividualPage.dart
+++ lib/screens/IndividualPage.dart
@@ -14,17 +14,21 @@ class IndividualPageState extends State<IndividualPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
         leadingWidth: 70,
         titleSpacing: 0,
         leading: InkWell(
-          onTap: (){
-        Navigator.pop(context);
-        },
+          onTap: () {
+            Navigator.pop(context);
+          },
           child: Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: const [
-              Icon(Icons.arrow_back_ios_new, size: 24,),
+              Icon(
+                Icons.arrow_back_ios_new,
+                size: 24,
+              ),
               CircleAvatar(
                 radius: 20,
                 backgroundColor: Colors.blueGrey,
@@ -34,49 +38,111 @@ class IndividualPageState extends State<IndividualPage> {
           ),
         ),
         title: InkWell(
-          onTap: (){},
+          onTap: () {},
           child: Container(
             margin: const EdgeInsets.all(10),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(widget.chatModel.name, style: const TextStyle(fontSize: 18.5, fontWeight: FontWeight.bold),),
-                const Text("Last seen today at 69:69", style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),),
+                Text(
+                  widget.chatModel.name,
+                  style: const TextStyle(
+                      fontSize: 18.5, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  "Last seen today at 69:69",
+                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+                ),
               ],
             ),
           ),
         ),
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.videocam)),
-          IconButton(onPressed: (){}, icon: const Icon(Icons.call)),
-          PopupMenuButton<String>(onSelected: (value) {
-          }, itemBuilder: (BuildContext context) {
-            return [
-              const PopupMenuItem(
-                value: "View Contact",
-                child: Text("View Contact"),
-              ),
-              const PopupMenuItem(
-                value: "Media, links and docs",
-                child: Text("Media, links and docs"),
-              ),
-              const PopupMenuItem(
-                value: "Search",
-                child: Text("Search"),
-              ),
-              const PopupMenuItem(
-                value: "Wallpaper",
-                child: Text("Wallpaper"),
-              ),
-              const PopupMenuItem(
-                value: "Settings",
-                child: Text("Settings"),
-              ),
-            ];
-          })
+          IconButton(onPressed: () {}, icon: const Icon(Icons.videocam)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
+          PopupMenuButton<String>(
+              onSelected: (value) {},
+              itemBuilder: (BuildContext context) {
+                return [
+                  const PopupMenuItem(
+                    value: "View Contact",
+                    child: Text("View Contact"),
+                  ),
+                  const PopupMenuItem(
+                    value: "Media, links and docs",
+                    child: Text("Media, links and docs"),
+                  ),
+                  const PopupMenuItem(
+                    value: "Search",
+                    child: Text("Search"),
+                  ),
+                  const PopupMenuItem(
+                    value: "Wallpaper",
+                    child: Text("Wallpaper"),
+                  ),
+                  const PopupMenuItem(
+                    value: "Settings",
+                    child: Text("Settings"),
+                  ),
+                ];
+              })
         ],
-
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            ListView(),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width - 55,
+                    child: Card(
+                      margin: const EdgeInsets.only(left: 10, right: 10, bottom: 5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: TextFormField(
+                        textAlignVertical: TextAlignVertical.center,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 5,
+                        minLines: 1,
+                        decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.all(10),
+                            prefixIcon: IconButton(
+                                icon: const Icon(Icons.emoji_emotions),
+                                onPressed: () {},
+                            ),
+                            suffixIcon: Row(
+                              // mainAxisAlignment: MainAxisAlignment.end,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(onPressed: (){}, icon: const Icon(Icons.attach_file_rounded),),
+                                IconButton(onPressed: (){}, icon: const Icon(Icons.camera_alt_rounded))
+                              ],
+                            ),
+                            border: InputBorder.none,
+                            hintText: "Type a message"),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 5),
+                    child: CircleAvatar(
+                      radius: 22,
+                      backgroundColor: Colors.blue,
+                      child: Icon(Icons.mic, color: Colors.white,),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
