9d6dc457a0de276f22ba4b97a000f53bf48baa81
lalit <alalitkumar02@gmail.com>
2023-06-25 12:55:58 +0530

    added service layer for user

--- backend/internal/user/user.go
+++ backend/internal/user/user.go
@@ -12,3 +12,19 @@ type User struct {
 type Repository interface {
 	CreateUser(ctx context.Context, user *User) (*User, error)
 }
+
+type CreateUserRequest struct {
+	ID       int64  `json:"id" db:"id"`
+	UserName string `json:"userName" db:"username"`
+	Email    string `json:"email" db:"email"`
+}
+
+type CreateUserResponse struct {
+	ID       int64  `json:"id" db:"id"`
+	UserName string `json:"userName" db:"username"`
+	Email    string `json:"email" db:"email"`
+}
+
+type Service interface {
+	CreateUser(ctx context.Context, request *CreateUserRequest) (*CreateUserResponse, error)
+}
--- backend/internal/user/user_repository.go
+++ backend/internal/user/user_repository.go
@@ -23,5 +23,11 @@ func newRepository(db DBTX) Repository {
 }
 
 func (r *repository) CreateUser(ctx context.Context, user *User) (*User, error) {
+	var lastInsertedId int
+	query := "INSERT INTO users(username, password, email) VALUES ($1, $2, $3) returning id"
+	if err := r.db.QueryRowContext(ctx, query, user.UserName, user.Password, user.Email).Scan(lastInsertedId); err != nil {
+		return nil, err
+	}
+	user.ID = int64(lastInsertedId)
 	return nil, nil
 }
